9f7990f96dca970e5ecd45756a1cdce67112f9c4
Trfhshdj8654 <Pryog997@outlook.com>
2024-08-03 17:03:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("29702990", ""))
-API_HASH = getenv("729bd1795d8dece58935af48cb4fa12a", "")
-BOT_TOKEN = getenv("7266054731:AAFgvsvHMZjWzBuPCDXD7B7v8rall9x1hyk", "")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
